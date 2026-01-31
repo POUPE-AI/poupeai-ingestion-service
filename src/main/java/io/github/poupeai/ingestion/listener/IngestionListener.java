9b47d41c3e51e1ff6f306d74c46c69a3094332a2
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2026-01-31 18:20:17 -0300

    feat: integrate OFX parser into ingestion listener

--- src/main/java/io/github/poupeai/ingestion/listener/IngestionListener.java
+++ src/main/java/io/github/poupeai/ingestion/listener/IngestionListener.java
@@ -1,13 +1,16 @@
 package io.github.poupeai.ingestion.listener;
 
 import io.github.poupeai.ingestion.domain.event.IngestionEvent;
+import io.github.poupeai.ingestion.domain.model.BankTransaction;
 import io.github.poupeai.ingestion.service.StorageService;
+import io.github.poupeai.ingestion.service.parser.OfxParserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -15,25 +18,33 @@ import java.io.InputStream;
 public class IngestionListener {
 
     private final StorageService storageService;
+    private final OfxParserService ofxParserService;
 
     @RabbitListener(queues = "${app.rabbitmq.queue.ingestion}")
     public void handleIngestionEvent(IngestionEvent event) {
-        log.info("Recebendo evento de ingestão. ID: {}, Type: {}", event.messageId(), event.eventType());
+        log.info("Recebendo evento de ingestão. ID: {}", event.messageId());
 
         if (event.payload() == null) {
-            log.warn("Evento recebido sem payload. Ignorando.");
+            log.warn("Payload nulo. Ignorando.");
             return;
         }
 
         String fileKey = event.payload().fileKey();
-        log.info("Tentando processar arquivo: {}", fileKey);
+        log.info("Iniciando processamento do arquivo OFX: {}", fileKey);
 
         try (InputStream inputStream = storageService.downloadFile(fileKey)) {
-            log.info("SUCESSO! Arquivo acessado. Bytes disponíveis estimados: {}", inputStream.available());
 
+            List<BankTransaction> transactions = ofxParserService.parse(inputStream);
+
+            log.info("Sucesso! Parse OFX realizado. Foram encontradas {} transações válidas.", transactions.size());
+
+           transactions.forEach(t -> log.info("Banco: {} | Data: {} | Valor: {} | FITID: {} | Desc: {}",
+                    t.getBankCode(), t.getDate().toLocalDate(), t.getAmount(), t.getFitId(), t.getDescription()));
+
+            // TODO: Próxima issue -> Enviar para Core Service
 
         } catch (Exception e) {
-            log.error("Erro ao processar ingestão", e);
+            log.error("Erro ao processar ingestão OFX", e);
         }
     }
 }
