9b0f099c195bc24708a28bf7cd25127a5e5100d2
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2026-01-31 18:19:56 -0300

    feat: add BankTransaction model

---
+++ src/main/java/io/github/poupeai/ingestion/domain/model/BankTransaction.java
@@ -0,0 +1,18 @@
+package io.github.poupeai.ingestion.domain.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class BankTransaction {
+    private String fitId;
+    private String bankCode;
+    private LocalDateTime date;
+    private BigDecimal amount;
+    private String description;
+    private String type;
+}
